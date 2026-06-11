d6145536e140ccaa4914e1c964c239f02d084fd8
yuandongx <yuandongx@126.com>
2026-06-11 13:12:48 +0800

    U

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ from app.server import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    run(app, host="0.0.0.0", port=8000, reload=True)
+    run("main:app", host="0.0.0.0", port=8000, reload=True)
