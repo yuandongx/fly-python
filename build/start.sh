9c315ee46151d82bbd03f054efd0efd13b63f9c3
yuandongx <yuandongx@126.com>
2026-06-23 16:08:08 +0800

    fix

--- build/start.sh
+++ build/start.sh
@@ -6,14 +6,15 @@
 
 # 目标服务名称
 PROJECT_NAME="dev"
-TARGET_SERVICE1="$PROJECT_NAME\_api_1"
-TARGET_SERVICE2="$PROJECT_NAME\_worker_1"
+TARGET_SERVICE1="$PROJECT_NAME"_api_1
+TARGET_SERVICE2="$PROJECT_NAME"_worker_1
 
 # 检查是否正在运行着目标服务, 如果是, 则停止它们
-docker stop -f $(docker ps -q -f name="$TARGET_SERVICE1")
-docker stop -f $(docker ps -q -f name="$TARGET_SERVICE2")
+docker rm -f $(docker ps -q -f name="$TARGET_SERVICE1")
+docker rm -f $(docker ps -q -f name="$TARGET_SERVICE2")
 # 到上一层目录
 cd "$(dirname "$0")/.." || exit 1
+ls -l
 
 # 启动目标服务
 echo "Starting services..."
