2c2e8f59feca64979bf7f47f7cd4fb96cfcc47ae
yuandongx <yuandongx@126.com>
2026-06-23 16:10:56 +0800

    fix

--- build/start.sh
+++ build/start.sh
@@ -19,4 +19,4 @@ ls -l
 # 启动目标服务
 echo "Starting services..."
 # 启动服务
-docker-compose -p "$PROJECT_NAME" up -f build/docker-compose-api.yml -d 
+docker-compose -p "$PROJECT_NAME" -f build/docker-compose-api.yml up -d 
