03ea7aafcf807424ea091cf220dcf15d552b41e7
yuandongx <yuandongx@126.com>
2026-06-23 16:02:23 +0800

    U

--- build/start.sh
+++ build/start.sh
@@ -5,17 +5,19 @@
 # Usage: ./start.sh
 
 # 目标服务名称
-PROJECT_NAME="fly-dev"
-TARGET_SERVICE1="test-service"
-TARGET_SERVICE2="test-service"
+PROJECT_NAME="dev"
+TARGET_SERVICE1="$PROJECT_NAME\_api_1"
+TARGET_SERVICE2="$PROJECT_NAME\_worker_1"
 
 # 检查是否正在运行着目标服务, 如果是, 则停止它们
-if pgrep -x "$TARGET_SERVICE1" > /dev/null || pgrep -x "$TARGET_SERVICE2" > /dev/null; then
-    echo "Stopping existing services..."
-    pkill -f "$TARGET_SERVICE1"
-    pkill -f "$TARGET_SERVICE2"
-    echo "Existing services stopped."
-fi
+docker ps -q -f name="$TARGET_SERVICE1" | grep -q . && {
+    echo "Stopping existing service: $TARGET_SERVICE1"
+    docker-compose -p "$PROJECT_NAME" stop "$TARGET_SERVICE1"
+}
+docker ps -q -f name="$TARGET_SERVICE2" | grep -q . && {
+    echo "Stopping existing service: $TARGET_SERVICE2"
+    docker-compose -p "$PROJECT_NAME" stop "$TARGET_SERVICE2"
+}
 
 # 到上一层目录
 cd "$(dirname "$0")/.." || exit 1
