07f0eaabeeb1085ec1db370ccdda05c68c82ff76
yuandongx <yuandongx@126.com>
2026-06-23 16:05:30 +0800

    fix

--- build/start.sh
+++ build/start.sh
@@ -10,15 +10,8 @@ TARGET_SERVICE1="$PROJECT_NAME\_api_1"
 TARGET_SERVICE2="$PROJECT_NAME\_worker_1"
 
 # 检查是否正在运行着目标服务, 如果是, 则停止它们
-docker ps -q -f name="$TARGET_SERVICE1" | grep -q . && {
-    echo "Stopping existing service: $TARGET_SERVICE1"
-    docker-compose -p "$PROJECT_NAME" stop "$TARGET_SERVICE1"
-}
-docker ps -q -f name="$TARGET_SERVICE2" | grep -q . && {
-    echo "Stopping existing service: $TARGET_SERVICE2"
-    docker-compose -p "$PROJECT_NAME" stop "$TARGET_SERVICE2"
-}
-
+docker stop -f $(docker ps -q -f name="$TARGET_SERVICE1")
+docker stop -f $(docker ps -q -f name="$TARGET_SERVICE2")
 # 到上一层目录
 cd "$(dirname "$0")/.." || exit 1
 
